ed498271fb4d9dc2a6166268934ea85ce3a36265
ashokpnd12 <ashokpnd12@gmail.com>
2019-08-26 17:57:45 +0530

    added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 
 export default class Navbar extends Component{
     render(){
     return (
-          <h3>Hello from Navbar</h3>
+          <nav className="navbar navbar-expand-sm
+           bg-primary navbar-dark px-sm-5">
+           <Link to="/">
+             <img src={logo} alt="store" className="navbar-brand"/>
+           </Link>
+           <ul className="navbar-nav align-items-center">
+             <li className="nav-item ml-5">
+              <Link to="/" className="nav-link">
+                Products
+              </Link>
+             </li>
+           </ul>
+           <Link to="/cart" className="ml-auto">
+             <button>
+               <i className="fas fa-cart-plus"/>
+               cart
+             </button>
+           </Link>
+           </nav>
       );
     }
   }
