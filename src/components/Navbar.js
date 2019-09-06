7720ee0375844040a4ca75d57a5e753316008e1c
ashokpnd12 <ashokpnd12@gmail.com>
2019-09-06 16:34:47 +0530

    till context file creation 01:40

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import React, {Component} from "react";
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
 import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import {ButtonContainer} from './Button';
+import styled from 'styled-components';
 
 export default class Navbar extends Component{
     render(){
     return (
-          <nav className="navbar navbar-expand-sm
-           bg-primary navbar-dark px-sm-5">
+          <NavWrapper className="navbar navbar-expand-sm
+           navbar-dark px-sm-5">
            <Link to="/">
              <img src={logo} alt="store" className="navbar-brand"/>
            </Link>
@@ -19,12 +21,23 @@ export default class Navbar extends Component{
              </li>
            </ul>
            <Link to="/cart" className="ml-auto">
-             <button>
-               <i className="fas fa-cart-plus"/>
+             <ButtonContainer>
+               <span className="mr-2">
+                 <i className="fas fa-cart-plus"/>
+               </span>
                cart
-             </button>
+             </ButtonContainer>
            </Link>
-           </nav>
+           </NavWrapper>
       );
     }
   }
+
+  const NavWrapper=styled.nav`
+  background: var(--mainBlue);
+  .nav-link{
+    color:var(--mainWhite) !important;
+    font-size:1.3rem;
+    text-transform:capitalize;
+  }
+  `;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import {Link,NavLink,BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import {ProductProvider} from './components/Context';
 
 ReactDOM.render(
-<Router>
-    <App />
-    <Link exact to='/'>Home</Link>
-    <br/>
-    <NavLink activeClassName="text-bright" to="/cart">Back</NavLink>
-</Router>
+    <ProductProvider>
+        <Router>
+            <App />
+            <Link exact to='/'>Home</Link>
+            <br/>
+            <NavLink activeClassName="text-bright" to="/cart">Back</NavLink>
+        </Router>
+    </ProductProvider>
+
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
